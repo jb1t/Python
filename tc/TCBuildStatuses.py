a6d9b648bf58dc75478c73ae4dfad2f0c3b496bb
Jeff Garrison <jeff.l.garrison@gmail.com>
2013-07-24 14:59:16 -0700

    Will get the build status from TeamCity

---
+++ tc/TCBuildStatuses.py
@@ -0,0 +1,22 @@
+import requests, xmltodict
+
+tc_url = "http://pc21568:90/httpAuth/app/rest/cctray/projects.xml"
+response = requests.get(tc_url, auth=('username', 'password'))
+doc = xmltodict.parse(response.text)
+projects = doc['Projects']['Project']
+
+overallStatus = "Success"
+
+for project in projects:
+    projectName = project['@name']
+    buildStatus = project['@lastBuildStatus']
+    if buildStatus != 'Success' and buildStatus != 'Unknown' and projectName.count(":: CI") > 0:
+        overallStatus = "Failure"
+        #print(projectName, buildStatus)
+        break
+
+print(overallStatus)
+    
+
+
+
