64cab6cc54bdf758e92e69c9aa72d0da317d4589
Jeff Garrison <jeff.l.garrison@gmail.com>
2017-12-30 10:51:19 -0800

    Renamed folder from tc to buildlights, and changed GPIO setup from BOARD to BCM

--- buildlights/LightProvider.py
+++ buildlights/LightProvider.py
@@ -8,15 +8,15 @@ except ImportError:
 class LightProvider:
     @staticmethod
     def GREEN_PIN():
-        return 7
+        return 2
 
     @staticmethod
     def YELLOW_PIN():
-        return 11
+        return 3
 
     @staticmethod
     def RED_PIN():
-        return 12
+        return 4
 
     def __init__(self, GPIO):
         self.GPIO = GPIO
@@ -29,7 +29,7 @@ class LightProvider:
 
     def setupGPIO(self):
         print 'setup the GPIO'
-        self.GPIO.setmode(self.GPIO.BOARD)
+        self.GPIO.setmode(self.GPIO.BCM)
         self.GPIO.setup(LightProvider.GREEN_PIN(), self.GPIO.OUT)
         self.GPIO.setup(LightProvider.YELLOW_PIN(), self.GPIO.OUT)
         self.GPIO.setup(LightProvider.RED_PIN(), self.GPIO.OUT)
