b6d06dad8535506ecf90323d9114fb2c174d958c
Jeff Garrison <jeff.l.garrison@gmail.com>
2023-02-07 19:54:48 -0800

    Sending and receiving azure iot messages

--- stranger_lights/send_message.py
+++ stranger_lights/send_message.py
@@ -6,45 +6,36 @@
 
 import os
 import asyncio
+import uuid
 from azure.iot.device.aio import IoTHubDeviceClient
+from azure.iot.device import Message
+
+messages_to_send = 10
 
 
 async def main():
     # The connection string for a device should never be stored in code. For the sake of simplicity we're using an environment variable here.
     conn_str = os.getenv("IOTHUB_DEVICE_CONNECTION_STRING")
+
     # The client object is used to interact with your Azure IoT hub.
     device_client = IoTHubDeviceClient.create_from_connection_string(conn_str)
 
-    # connect the client.
+    # Connect the client.
     await device_client.connect()
 
-    # define behavior for receiving a message
-    # NOTE: this could be a function or a coroutine
-    def message_received_handler(message):
-        print("the data in the message received was ")
-        print(message.data)
-        print("custom properties are")
-        print(message.custom_properties)
-        print("content Type: {0}".format(message.content_type))
-        print("")
+    async def send_test_message(i):
+        print("sending message #" + str(i))
+        msg = Message("test wind speed " + str(i))
+        msg.message_id = uuid.uuid4()
+        msg.correlation_id = "correlation-1234"
+        msg.custom_properties["tornado-warning"] = "yes"
+        msg.content_encoding = "utf-8"
+        msg.content_type = "application/json"
+        await device_client.send_message(msg)
+        print("done sending message #" + str(i))
 
-    # set the message received handler on the client
-    device_client.on_message_received = message_received_handler
-
-    # define behavior for halting the application
-    def stdin_listener():
-        while True:
-            selection = input("Press Q to quit\n")
-            if selection == "Q" or selection == "q":
-                print("Quitting...")
-                break
-
-    # Run the stdin listener in the event loop
-    loop = asyncio.get_running_loop()
-    user_finished = loop.run_in_executor(None, stdin_listener)
-
-    # Wait for user to indicate they are done listening for messages
-    await user_finished
+    # send `messages_to_send` messages in parallel
+    await asyncio.gather(*[send_test_message(i) for i in range(1, messages_to_send + 1)])
 
     # Finally, shut down the client
     await device_client.shutdown()
